21d9d005f11ec91167a63c5785016f8e4d0c73c9
Dror Eiger <45061021+deiger@users.noreply.github.com>
2022-01-31 17:29:24 +0200

    Bump paho-mqtt version to 1.6.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     license='GPL 3.0',
     packages=setuptools.find_packages(),
     install_requires=[
-        'aiohttp==3.7.4', 'dataclasses_json', 'pycryptodome', 'paho-mqtt==1.5.0', 'tenacity',
+        'aiohttp==3.7.4', 'dataclasses_json', 'pycryptodome', 'paho-mqtt==1.6.1', 'tenacity',
         'get-mac', 'retry'
     ],
     classifiers=[
