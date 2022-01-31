a30f6092268119079aa9bda2dc984db8c75c6f29
Dror Eiger <45061021+deiger@users.noreply.github.com>
2022-01-31 18:07:40 +0200

    0.3.10

--- aircon/__init__.py
+++ aircon/__init__.py
@@ -1,2 +1,2 @@
 from . import *
-__version__ = '0.3.9'
+__version__ = '0.3.10'
--- new_version.sh
+++ new_version.sh
@@ -11,7 +11,7 @@ else
   NEW_VERSION_MSG=$2
 fi
 
-git tag -a $NEW_VERSION -m $NEW_VERSION_MSG
+git tag -a $NEW_VERSION -m "$NEW_VERSION_MSG"
 auto-changelog
 
 for f in aircon/__init__.py hassio/config.json docker-compose.yaml; do
@@ -20,7 +20,7 @@ done
 
 git commit -a -m $NEW_VERSION
 git tag -d $NEW_VERSION
-git tag -a $NEW_VERSION -m $NEW_VERSION_MSG
+git tag -a $NEW_VERSION -m "$NEW_VERSION_MSG"
 git push
 docker buildx build --platform linux/arm/v7,linux/arm64,linux/amd64,linux/386 -t deiger/aircon:$NEW_VERSION --push .
 git push --tags
