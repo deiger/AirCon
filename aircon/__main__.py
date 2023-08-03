e79b6dd6bba4a1a0ae05f5f91f284921bb78e668
Dror Eiger <45061021+deiger@users.noreply.github.com>
2023-08-03 17:22:57 +0300

    Clear the entity name in order to conform with HA 2023.8
    Fixes #248

--- aircon/__main__.py
+++ aircon/__main__.py
@@ -194,7 +194,7 @@ async def run(parsed_args):
     mqtt_client.publish(mqtt_topics['lwt'], payload='online', retain=True)
     for device in devices:
       config = {
-          'name': device.name,
+          'name': None,
           'unique_id': device.mac_address,
           'device': {
               'identifiers': [f'hisense_ac_{device.mac_address}'],
